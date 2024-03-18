5979675e2b288ef204c81dbf13fd8f9cb6e6139b
Muhammad <my786@hotmail.com>
2024-03-18 14:18:57 -0400

    to delete extra file

--- src/main/java/org/acme/getting/started/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package org.acme.getting.started.repository;
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-
-@ApplicationScoped
-public class PersonRepository implements PanacheMongoRepository<Person> {
-    // You can add custom methods here if needed
-}
